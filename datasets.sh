de11faefa9f83010b5cb20d696b6af421adb8b83
xriss <kriss@xixs.com>
2021-09-15 17:34:29 +0100

    use d-portal to join xml files and remove duplicates

--- datasets.sh
+++ datasets.sh
@@ -4,6 +4,10 @@ cd "$dirname"
 # This script will recreate datasets from our individual activitiy files by joining them together
 
 
+rm -rf node_modules
+npm install d-portal
+rm -rf package*
+
 
 # create or replace our datasets directory
 
@@ -11,15 +15,5 @@ rm -rf datasets
 mkdir -p datasets
 
 
-# join all the files together, skipping the first line which is just an xml header
-
-funct() {
-declare -a 'a=('"$1"')'
-file=${a[0]}
-
-	tail -n+2 -q xml/$file/*.xml >datasets/$file.xml
-
-}
-export -f funct
-ls -1 xml | sort -R | parallel -j 32 --bar funct
+node_modules/d-portal/dflat/dflat packages-join --dir "$dirname" --dedupe
 
