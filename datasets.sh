8f3bb7dcabc06b4ebb9a903d227509e155f4321c
xriss <kriss@xixs.com>
2021-11-16 14:42:13 +0000

    faster datasets join

--- datasets.sh
+++ datasets.sh
@@ -1,19 +1,33 @@
-dirname=$( dirname "$(readlink -f "$0")" )
-cd "$dirname"
 
 # This script will recreate datasets from our individual activitiy files by joining them together
+# and removing all duplicate activities so each id is only used once
 
+export dirname=$( dirname "$(readlink -f "$0")" )
+cd "$dirname"
+
+if ! [ -x "$(command -v parallel)" ]; then
+	echo "parallel is not installed, atempting to install"
+	sudo apt install -y parallel
+fi
 
 rm -rf node_modules
 npm install d-portal
 rm -rf package*
 
 
-# create or replace our datasets directory
+dodataset() {
+declare -a 'a=('"$1"')'
+slug=${a[0]}
 
+echo Creating unique data only dataset for $slug
+
+node_modules/d-portal/dflat/dflat --dir $dirname packages-join $slug --dedupe 2>&1
+
+}
+export -f dodataset
+
+# deleteing all datasets as we will be rebuilding all of them
 rm -rf datasets
-mkdir -p datasets
 
-
-node_modules/d-portal/dflat/dflat packages-join --dir "$dirname" --dedupe
+ls -1 xml | sort -R | parallel -j -1 --bar dodataset
 
